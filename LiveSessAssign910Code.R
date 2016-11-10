ce27d6d49634909d2040a9a776004774537694b9
jcatam1999 <jcatam99@gmail.com>
2016-11-09 21:39:11 -0600

    stuff

--- LiveSessAssign910Code.R
+++ LiveSessAssign910Code.R
@@ -1,12 +1,12 @@
 ##get the data
-FilePath = 'C:\\RCode\\LiveSession10_9Assignment.git\\CHF-master\\CHF.csv'
+FilePath = 'C:\\RProjects\\LiveSession10_9Assignment\\CHF.csv'
 Hdata = read.csv(FilePath)
 head(Hdata)
 
 ##Set Gender variable
-Hdata[data$Sex == 0, "Gender"] = 'Unknown'
-Hdata[data$Sex == 1, "Gender"] = 'Male'
-Hdata[data$Sex == 2, "Gender"] = 'Female'
+Hdata$Gender[Hdata$Sex == 0] = 'Unknown'
+Hdata$Gender[Hdata$Sex == 1] = 'Male'
+Hdata$Gender[Hdata$Sex == 2] = 'Female'
 
 ##Create age category
 Hdata$agecat[Hdata$Age ==1] = 'Less than 25'
@@ -19,6 +19,17 @@ Hdata$agecat[Hdata$Age ==7] = '80-84'
 Hdata$agecat[Hdata$Age ==8] = '85-89'
 Hdata$agecat[Hdata$Age ==9] = '90 and over'
 
+##
+Hdata$admsrcdesc[Hdata$admsrc == 1] = 'Physician referral'
+Hdata$admsrcdesc[Hdata$admsrc == 2] = 'Clinic referral'
+Hdata$admsrcdesc[Hdata$admsrc == 3] = 'HMO referral'
+Hdata$admsrcdesc[Hdata$admsrc == 4] = 'Trans(Hospital)'
+Hdata$admsrcdesc[Hdata$admsrc == 5] = 'Trans(Nurse Fac) '
+Hdata$admsrcdesc[Hdata$admsrc == 6] = 'Trans(Healthcare fac)'
+Hdata$admsrcdesc[Hdata$admsrc == 7] = 'Emergency room'
+Hdata$admsrcdesc[Hdata$admsrc == 8] = 'Court/law enforcement'
+Hdata$admsrcdesc[Hdata$admsrc == 9] = 'Info not avail'
+
 ##Show the Mean
 mean(Hdata$AmtReim)
 mean(Hdata$TotDeptChg)
@@ -29,13 +40,13 @@ sd(Hdata$AmtReim)
 sd(Hdata$TotDeptChg)
 sd(Hdata$TotAccomChg)
 
+##boxplot
 
 ##Tapply mean
 tapply(Hdata$AmtReim, Hdata$Gender, mean)
 tapply(Hdata$TotAccomChg, Hdata$Gender, mean)
 tapply(Hdata$TotDeptChg, Hdata$Gender, mean)
 
-##boxplot
 boxplot(Hdata$AmtReim~Hdata$Gender, data=Hdata, main="Amount Paid By Medicare", col=(c("yellow","blue")),xlab="Gender", ylab="Dollars")
 boxplot(Hdata$TotAccomChg~Hdata$Gender, data=Hdata, main="Total Accomodation Charges", col=(c("yellow","blue")),xlab="Gender", ylab="Dollars")
 boxplot(Hdata$TotDeptChg~Hdata$Gender, data=Hdata, main="Total Department Charges", col=(c("yellow","blue")),xlab="Gender", ylab="Dollars")
@@ -46,4 +57,14 @@ hist(Hdata$Sex)
 hist(Hdata$TotAccomChg)
 
 ##Barplots
-cnt=table(Hdata)
+cnt=table(Hdata$Gender,Hdata$admsrcdesc)
+par(mar=c(13.5, 2.1 ,2.1 ,2.1))
+barplot(cnt, main='Admission Source By Gender', xlab='Admission Source', 
+        col=c('blue', 'red'), legend = rownames(cnt), beside = TRUE, ylim = c(0,110), las=2)
+
+##t test
+Hdata291 = subset(Hdata, Hdata$drgcode ==291)
+head(Hdata)
+mean(Hdata291$LOS)
+hist(Hdata291$LOS)
+t.test(Hdata291$LOS, mu=6, alternative = 'two.sided')
